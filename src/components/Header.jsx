f44ed45dd428b398c091224778ad8ac1ad188498
Adolfo Neto <adolfo.usp@gmail.com>
2023-09-09 17:38:08 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-bimport { Button } from '@/components/Button'
+import { Button } from '@/components/Button'
 import { Container } from '@/components/Container'
 import { DiamondIcon } from '@/components/DiamondIcon'
 import { Logo } from '@/components/Logo'
