c4555742090037321a0b6458a333d913b052c486
Cristine Guadelupe <cristineguadelupe@me.com>
2023-09-11 09:52:01 +0700

    Add google analytics

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,10 +1,23 @@
 import { Hero } from '@/components/Hero'
 import { Schedule } from '@/components/Schedule'
 import { Speakers } from '@/components/Speakers'
+import Script from 'next/script'
 
 export default function Home() {
   return (
     <>
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JF32CGGFV1" />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JF32CGGFV1')
+          `}
+        </Script>
+      </head>
       <Hero />
       <Speakers />
       <Schedule />
