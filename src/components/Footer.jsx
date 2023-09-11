3dd915b6f1c39dc0916499370eea402cd6b5ff4c
Adolfo Neto <adolfo.usp@gmail.com>
2023-09-11 14:19:13 -0300

    Em caso de dúvida, envie email para elixiremfoco@gmail.com.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,9 @@ export function Footer() {
       <Container className="flex flex-col items-center justify-between md:flex-row">
         <Logo className="h-12 w-auto text-slate-900" />
         <p className="mt-6 text-base text-slate-500 md:mt-0">
-          Desenvolvido e mantido por <a className='text-purple-550' href='http://elixiremfoco.com'>Elixir em Foco</a>
+          Desenvolvido e mantido por 
+          <a className='text-purple-550' href='http://elixiremfoco.com'>Elixir em Foco</a>. 
+          Em caso de dúvida, envie email para elixiremfoco@gmail.com.
         </p>
       </Container>
     </footer>
