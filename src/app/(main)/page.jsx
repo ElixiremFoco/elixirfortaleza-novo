eb7b2ff56f7b7fb0d8720ed2e669bcddf11260fa
Cristine Guadelupe <cristineguadelupe@me.com>
2023-09-11 09:58:33 +0700

    Format

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -6,18 +6,19 @@ import Script from 'next/script'
 export default function Home() {
   return (
     <>
-      <head>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-JF32CGGFV1" />
-        <Script id="google-analytics">
-          {`
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-JF32CGGFV1"
+      />
+      <Script id="google-analytics">
+        {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
             gtag('config', 'G-JF32CGGFV1')
           `}
-        </Script>
-      </head>
+      </Script>
       <Hero />
       <Speakers />
       <Schedule />
