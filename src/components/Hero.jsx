ee69d2d70c5ca70c4ab4ae6ef873d330d373fea2
Adolfo Neto <adolfo.usp@gmail.com>
2023-09-12 19:39:26 -0300

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,7 +49,7 @@ function portuguese() {
             <div className="order-first -mx-4 550/10 py-4">
               <div className="mx-auto flex items-center gap-4 px-4 font-mono text-md">
                 <DiamondIcon className="h-1.5 w-1.5 overflow-visible fill-current stroke-current" />
-                <p><a className='text-purple-550' href="https://www.youtube.com/watch?v=yBT29ADYiWc">&quot;Esquenta&quot; do Elixir Fortaleza</a>:</p>
+                <p><a className='text-purple-550' href="https://www.youtube.com/watch?v=yBT29ADYiWc">&quot;Esquenta&quot; do Elixir Fortaleza (online no YouTube)</a>:</p>
                 <p className='text-sm'>15 de setembro, à noite</p>
               </div>
               <div className="mx-auto flex items-center gap-4 px-4 font-mono text-md">
